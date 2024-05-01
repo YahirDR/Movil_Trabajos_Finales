fba5235fb0ed5d95e40b8fbbdc11a6b97ef1d147
yahir <s20120196@alumnos.itsur.edu.mx>
2024-04-30 18:37:39 -0600

    Se agrego lo siguiente en el MainActivity.kt:
    Solicita permisos de notificación en dispositivos con Android 13  o superior.
    Obtiene el token de registro de Firebase Cloud Messaging (FCM) y lo muestra en un Toast.

--- PushNotification/app/src/main/java/com/example/pushnotification/MainActivity.kt
+++ PushNotification/app/src/main/java/com/example/pushnotification/MainActivity.kt
@@ -1,32 +1,76 @@
 package com.example.pushnotification
 
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.pushnotification.ui.theme.PushNotificationTheme
+import androidx.core.content.ContextCompat
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 
 
 class MainActivity : ComponentActivity() {
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // Permisos concedidos
+            Log.d("body", "granted")
+        } else {
+            // Permission denegados
+            Log.d("permission", "denied")
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            PushNotificationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+        // Se llama al método para solicitar permisos de notificación.
+        askNotificationPermission()
+        // Se obtiene el token de registro de Firebase Cloud Messaging (FCM).
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    // Si no se pudo obtener el token, se imprime un mensaje de log con el error.
+                    Log.d("FCM Notify", "Fetching FCM registration token failed", task.exception)
+                    return@OnCompleteListener
                 }
+
+                // Si se obtuvo el token, se muestra en un Toast y se imprime un mensaje de log.
+                val token: String? = task.result
+                Log.d("FCM Token", token, task.exception)
+                Toast.makeText(this, token, Toast.LENGTH_SHORT).show()
+            })
+
+        setContent {
+
+            Greeting("token")
+
+        }
+
+    }
+    // Método para solicitar permisos de notificación.
+    private fun askNotificationPermission() {
+        // Verifica si se necesita solicitar permisos de notificación en dispositivos con Android 13 o superior.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                // Si el permiso ya está concedido, no se hace nada.
+            } else if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
+                //Aqui se puede agregar más contenido
+            } else {
+                // Preguntar directamente los permisos
+                requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
             }
         }
     }
@@ -35,17 +79,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "App de notificaciones:  $name!",
         modifier = modifier
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PushNotificationTheme {
-        Greeting("Android")
-    }
-}
 
 
